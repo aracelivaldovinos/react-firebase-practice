1cbb9d5cb625c8be9b65c9266478be348c2a4f74
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-28 11:33:14 -0500

    set up local state for selected file and errors

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { useState } from "react";
+
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+
   const changeHandler = (e) =>{
+    e.preventDefault();
     let selected = e.target.files[0];
     console.log(selected)
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError('')
+    }else{
+      setFile(null);
+      setError('Please select an image file (png or jpeg)')
+
+    }
   }
 
   return ( 
     <div className="upload-form">
       <form>
-        <input type="file" onChange={changeHandler}/>
+        <input type="file" onChange={changeHandler} />
+        <div className="output">
+          {error && <div className="error"> {error}</div>}
+          {file && <div className="file"> {file.name}</div>}
+        </div>
       </form>
     </div>
    );
