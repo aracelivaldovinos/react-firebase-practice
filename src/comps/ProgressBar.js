a81e6332edaa5e22e28118b4ea5dc10666347958
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-11-01 10:32:27 -0500

    add framer motion to modal and progress bar comp

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useStorage from '../hooks/useStorage';
 import { useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({file, setFile}) => {
   const { url, progress } = useStorage(file);
@@ -11,8 +12,11 @@ const ProgressBar = ({file, setFile}) => {
     }
   }, [url, setFile])
   return ( 
-    <div className="progress-bar" style={{width: progress + '%'}}>
-    </div>
+    <motion.div className="progress-bar"
+    initial={{width: 0}}
+    animate={{width: progress + '%'}}
+    >
+    </motion.div>
    );
 }
  
